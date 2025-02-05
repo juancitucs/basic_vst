4075f74441534634abffb73d12bf810cd713d9b5
juancitucs <juancitolinuxlover@gmail.com>
2025-02-05 01:07:09 -0500

    error

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod plugin;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use plugin:GainPlugin;
+use vst::plugin_main;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+plugin_main!(GainPlugin);
---
+++ src/plugins.rs
@@ -0,0 +1,65 @@
+use vst::buffer::AudioBuffer;
+use vst::plugin::{Info, Plugin};
+use vst::plugin_main;
+
+// Struct that will hold the plugin state
+struct GainPlugin {
+    gain: f32,
+}
+
+impl Default for GainPlugin {
+    fn default() -> Self {
+        GainPlugin { gain: 0.5 } // Default gain
+    }
+}
+
+impl Plugin for GainPlugin {
+    fn get_info(&self) -> Info {
+        Info {
+            name: "BasicGainPlugin".to_string(),
+            vendor: "YourName".to_string(),
+            unique_id: 243723073, // Random unique ID
+            version: 1,
+            inputs: 2,
+            outputs: 2,
+            parameters: 1,
+            ..Default::default()
+        }
+    }
+
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+        let (inputs, outputs) = buffer.split();
+
+        for (input_buffer, output_buffer) in inputs.iter().zip(outputs.iter_mut()) {
+            for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer.iter_mut()) {
+                *output_sample = *input_sample * self.gain;
+            }
+        }
+    }
+
+    fn set_parameter(&mut self, index: i32, value: f32) {
+        if index == 0 {
+            // We only have one parameter
+            self.gain = value;
+        }
+    }
+
+    fn get_parameter(&self, index: i32) -> f32 {
+        if index == 0 {
+            self.gain
+        } else {
+            0.0
+        }
+    }
+
+    fn get_parameter_name(&self, index: i32) -> String {
+        if index == 0 {
+            "Gain".to_string()
+        } else {
+            "".to_string()
+        }
+    }
+}
+
+// Macro to make this a VST plugin
+plugin_main!(GainPlugin);
